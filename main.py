0ac96de0ab197fa6a117b1acb2d45ad39cf81d63
Maxpou5 <117453536+Maxpou5@users.noreply.github.com>
2023-11-16 16:57:04 +0100

    dkfjvlfv

--- main.py
+++ main.py
@@ -15,8 +15,15 @@ L.append(nom_pres(titre))
 titre = "Nomination_Macron.txt"
 L.append(nom_pres(titre))
 
-titre = "Nomination_Miterrand1.txt"
+titre = "Nomination_Mitterrand1.txt"
 L.append(nom_pres(titre))
 
 titre = "Nomination_Mitterrand2.txt"
 L.append(nom_pres(titre))
+
+liste_sans_doublons = []
+for element in L:
+    if element not in liste_sans_doublons:
+        liste_sans_doublons.append(element)
+
+print(liste_sans_doublons)
