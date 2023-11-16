1dfe235accaae5ca35e175e3746ba12a93216f88
EnzoCa8 <enzo.cacote@efrei.net>
2023-11-16 16:01:20 +0100

    maj

---
+++ function.py
@@ -0,0 +1,28 @@
+def recup_nom_president(nom_fichier):
+
+    '''On supprime le .txt du nom du fichier'''
+    nom_sans_extension = os.path.splitext(nom_fichier)[0]
+
+    '''Diviser le nom du fichier en parties en utilisant '_' comme séparateur'''
+    parties_nom = nom_sans_extension.split('_')
+
+    '''Récupère le nom du président, qui est la deuxième partie du nom du fichier'''
+    nom_president = parties_nom[1]
+
+    return nom_president
+
+def prenom_president():
+
+    if recup_nom_president(nom_fichier) == "Chirac" :
+        return "Jaques"
+    elif recup_nom_president(nom_fichier) == "Giscard" :
+        return "Valérie"
+    elif recup_nom_president(nom_fichier) == "Hollande" :
+        return "François"
+    elif recup_nom_president(nom_fichier) == "Macron" :
+        return "Emmanuel"
+    elif recup_nom_president(nom_fichier) == "Mitterand" :
+        return "François"
+    elif recup_nom_president(nom_fichier) == "Sarkozy" :
+        return"Nicolas"
+
