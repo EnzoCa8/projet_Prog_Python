3c18b196d0ee9d2a0f246c6139d13dc463dd72bd
EnzoCa8 <enzo.cacote@efrei.net>
2023-11-16 16:07:35 +0100

    maj

--- function.py
+++ function.py
@@ -1,4 +1,13 @@
-def recup_nom_president(nom_fichier):
+import os
+def list_of_files(directory, extension):
+
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
+
+def recup_nom_president(files_names):
 
     '''On supprime le .txt du nom du fichier'''
     nom_sans_extension = os.path.splitext(nom_fichier)[0]
