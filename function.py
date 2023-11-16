f39834ea082b42c77e5f7579c9e0e27a9566c07a
EnzoCa8 <enzo.cacote@efrei.net>
2023-11-16 16:32:52 +0100

    maj

--- function.py
+++ function.py
@@ -1,37 +1,23 @@
 import os
-def list_of_files(directory, extension):
 
-    files_names = []
-    for filename in os.listdir(directory):
-        if filename.endswith(extension):
-            files_names.append(filename)
-    return files_names
+def nom_pres(titre):
+    titre = titre[:len(titre)-4]
+    titre = titre[11:]
+    if ord(titre[len(nom_pres)-1]) <= 57:
+        titre = titre[:-1]
+    return titre
 
-def recup_nom_president(files_names):
-
-    '''On supprime le .txt du nom du fichier'''
-    nom_sans_extension = os.path.splitext(files_names)[0]
-
-    '''Diviser le nom du fichier en parties en utilisant '_' comme séparateur'''
-    parties_nom = nom_sans_extension.split('_')
-
-    '''Récupère le nom du président, qui est la deuxième partie du nom du fichier'''
-    nom_president = parties_nom[1]
-
-    return nom_president
-
-def prenom_president():
-
-    if recup_nom_president(nom_fichier) == "Chirac" :
-        return "Jaques"
-    elif recup_nom_president(nom_fichier) == "Giscard" :
-        return "Valérie"
-    elif recup_nom_president(nom_fichier) == "Hollande" :
-        return "François"
-    elif recup_nom_president(nom_fichier) == "Macron" :
-        return "Emmanuel"
-    elif recup_nom_president(nom_fichier) == "Mitterand" :
-        return "François"
-    elif recup_nom_president(nom_fichier) == "Sarkozy" :
-        return"Nicolas"
-
+def prenom_pres(nom):
+    prenom = ""
+    if nom == "Chirac":
+        prenom = "Jacques"
+    if nom == "Giscard d'Estaing":
+        prenom = "Valérie"
+    if nom == "Hollande":
+        prenom = "François"
+    if nom == "Macron":
+        prenom = "Emmanuel"
+    if nom == "Mitterand":
+        prenom = "François"
+    if nom == "Sarkozy":
+        prenom = "Nicolas"
