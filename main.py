7362236d07206629e8ae250db6fc487644910862
Maxpou5 <117453536+Maxpou5@users.noreply.github.com>
2023-11-18 18:29:05 +0100

    le reste des fichier

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ print(liste_sans_doublons)
 
 fichiers_dentree = ["Nomination_Chirac1.txt", "Nomination_Chirac2.txt", "Nomination_Giscard dEstaing.txt",
      "Nomination_Hollande.txt", "Nomination_Macron.txt", "Nomination_Mitterrand1.txt", "Nomination_Mitterrand2.txt", "Nomination_Sarkozy.txt"]
-lettres = ["ch1","ch2", "gd", "ho", "ma", "mi1", "mit2", "sa"]
+
 
 for fichier_entree in fichiers_dentree:
     lettre = lettres[i]
